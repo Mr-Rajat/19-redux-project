77f22485ab8ac269d8f5e346fbf7d46420150ac6
Rajat Choudhary <rajatch133@gmail.com>
2023-08-24 00:55:58 +0530

    added dispatch() using useDispatch() for reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,30 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  // we need to pass a fxn which will received the state managed by redux, then we return the part of the state we want to extract. when we used useSelector react redux automatically sets the subscription for this 
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  // we need to pass a fxn which will received the state managed by redux, then we return the part of the state we want to extract. when we used useSelector react redux automatically sets the subscription for this 
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  }
+
   const toggleCounterHandler = () => { };
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler} >Increment</button>
+        <button onClick={decrementHandler} >Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
