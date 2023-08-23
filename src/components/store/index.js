b6f99a0b43aed54403ff16fc952dd6ce2458084d
Rajat Choudhary <rajatch133@gmail.com>
2023-08-24 00:31:58 +0530

    reducer created and added in the createStore() as

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,6 +1,22 @@
 import { createStore } from 'redux'
 
+const counterReducer = (state = { counter: 0 }, action) => {
+    if(action.type === 'increment'){
+        return {
+            counter: state.counter + 1,
+        }
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter: state.counter - 1,
+        }
+    }
 
-const store = createStore();
+    // default case
+    return state;
+}
+
+// createStore wants a pointer at reducer fxn as a parameter
+const store = createStore(counterReducer);
 
 export default store;
