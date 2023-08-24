1187e176bec27df2926878e401148f048fb3bd87
Rajat Choudhary <rajatch133@gmail.com>
2023-08-25 00:33:39 +0530

    created store and passed reducer slice

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+// import { createStore } from 'redux';
+
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 // more enhance than the createReducer which is also available with toolkit
+// configure store also create a store like createStore but it makes merging multiple reducers into 1 reducer easier.
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
     name: "counter", // name of reducer
     initialState,  // using single value due to key and value have same name
     reducers: {
@@ -23,42 +25,50 @@ createSlice({
     }
 });
 
-const counterReducer = (state = initialState, action) => {
+const store = configureStore({
+    // expected property reducer
+    reducer: counterSlice.reducer
+});
 
-    if(action.type === 'increment'){
-        return {
-            // ...state,
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        }
-    }
 
-    if(action.type === 'increase'){
-        return {
-            counter: state.counter + action.payload,
-            showCounter: state.showCounter,
-        }
-    }
+// // Reducer for simple createStore with redux above is toolkit store
 
-    if(action.type === 'decrement'){
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        }
-    }
+// const counterReducer = (state = initialState, action) => {
 
-    if(action.type === 'toggle'){
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter,
-        }
-    }
+//     if(action.type === 'increment'){
+//         return {
+//             // ...state,
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter,
+//         }
+//     }
 
-    // default case
-    return state;
-}
+//     if(action.type === 'increase'){
+//         return {
+//             counter: state.counter + action.payload,
+//             showCounter: state.showCounter,
+//         }
+//     }
 
-// createStore wants a pointer at reducer fxn as a parameter
-const store = createStore(counterReducer);
+//     if(action.type === 'decrement'){
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         }
+//     }
+
+//     if(action.type === 'toggle'){
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter,
+//         }
+//     }
+
+//     // default case
+//     return state;
+// }
+
+// // createStore wants a pointer at reducer fxn as a parameter
+// const store = createStore(counterReducer);
 
 export default store;
